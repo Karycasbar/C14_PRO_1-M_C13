f5c5fa841557c095acf3cca8777e6e5e3924a5e4
Karycasbar <91565151+Karycasbar@users.noreply.github.com>
2024-06-25 18:24:40 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,10 +2,18 @@ var trex ,trex_running;
 var groundImage;
 var invisibleGround;
 var cloud, cloudImage;
+var obstacle, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");  
   groundImage = loadImage("ground2.png");
   cloudImage = loadImage("cloud.png");
+
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
 }
 
 function setup(){
@@ -49,7 +57,12 @@ function draw(){
   //evitar que el trex caiga
   trex.collide(invisibleGround);
 
+  //llamado de la función de nubes
   spawnClouds();
+
+//llamado de la función de obstaculos
+  spawnObstacles();
+
   drawSprites();
 }
 
@@ -60,5 +73,40 @@ function spawnClouds(){
   cloud.y = Math.round(random(10,60))
   cloud.scale = 0.4;
   cloud.velocityX = -3;
+
+  //ajustar la profundidad
+  cloud.depth = trex.depth;
+  trex.depth = trex.depth + 1;
+
+  //asignar ciclo de vida
+  cloud.lifetime = 210;
+  }
+}
+
+function spawnObstacles(){
+  if(frameCount % 60 === 0){
+    obstacle = createSprite(600, 165, 10, 40);
+    obstacle.velocityX = -6
+
+    //generar obstáculos al azar
+    var rand = Math.round(random(1,6));
+    switch(rand){
+      case 1: obstacle.addImage(obstacle1);
+      break;
+      case 2: obstacle.addImage(obstacle2);
+      break
+      case 3: obstacle.addImage(obstacle3);
+      break
+      case 4: obstacle.addImage(obstacle4);
+      break
+      case 5: obstacle.addImage(obstacle5);
+      break
+      case 6: obstacle.addImage(obstacle6);
+      break
+      default: break;
+    }
+    //asignar escala y tiempo de vida
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 210;
   }
 }
